06334994d3882c6a512874199eb8930431376599
lucaass16 <lucas16silva06@gmail.com>
2024-09-30 09:54:39 -0300

    Salvando tudo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
+const mainContent = document.getElementById('mainContent');
+
+// Função para abrir a sidebar
+toggleBtn.onclick = function() {
+    sidebar.classList.toggle('open'); // Alterna a classe 'open'
+    mainContent.classList.toggle('shift'); // Move o conteúdo principal
+}
+
+// Função para fechar a sidebar
+closeBtn.onclick = function() {
+    sidebar.classList.remove('open');
+    mainContent.classList.remove('shift');
+}
