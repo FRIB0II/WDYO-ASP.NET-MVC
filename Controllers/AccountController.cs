c0b4167ed5dbfdb7fcfca6e8373b6d894018be33
lucaass16 <lucas16silva06@gmail.com>
2024-10-11 10:03:08 -0300

    Adicionado página de conta de usuario

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhatDoYouOwn_ASPNET.Models;
+
+namespace WhatDoYouOwn_ASPNET.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
